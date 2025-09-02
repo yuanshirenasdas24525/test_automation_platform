a6a292ef41d2e4692fb1c70d9f4eea4faa192db3
Gui <9041355+ivogui@user.noreply.gitee.com>
2025-09-02 10:53:10 +0800

    增加删除目录下所有文件功能，删除缓存文件

--- src/runners/__init__.py
+++ src/runners/__init__.py
@@ -1,4 +1,6 @@
 from config.settings import ProjectPaths
-from src.utils.platform_utils import clear_log_files
+from src.utils.platform_utils import clear_log_files, delete_all_files_in_dir
 
 clear_log_files(ProjectPaths.LOG_DIR)
+# delete_all_files_in_dir(ProjectPaths.CACHE_FILE)
+# delete_all_files_in_dir(ProjectPaths.REPORT_DIR)
