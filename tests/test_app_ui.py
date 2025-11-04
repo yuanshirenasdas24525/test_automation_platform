de64d3df7797e0915018333d82650a2a74490b0c
seven <seven@gaogao888.tech>
2025-11-04 14:24:58 +0800

    ui 自動化内容

--- tests/test_app_ui.py
+++ tests/test_app_ui.py
@@ -16,25 +16,25 @@ class TestUiAutomatic:
 
     @pytest.mark.run(order=1)
     @pytest.mark.parametrize('cases', GenericCaseReader(ProjectPaths.ui_register_case, process_ui_row).read())
-    def test_android_uu_case(self, cases):
+    def test_android_register_case(self, cases):
         self.app.app_steps(cases)
 
     @pytest.mark.run(order=2)
     @pytest.mark.parametrize('cases', GenericCaseReader(ProjectPaths.ui_login_case, process_ui_row).read())
-    def test_android_uu_case(self, cases):
+    def test_android_login_case(self, cases):
         self.app.app_steps(cases)
 
     @pytest.mark.run(order=3)
     @pytest.mark.parametrize('cases', GenericCaseReader(ProjectPaths.ui_security_case, process_ui_row).read())
-    def test_android_uu_case(self, cases):
+    def test_android_security_case(self, cases):
         self.app.app_steps(cases)
 
     @pytest.mark.run(order=4)
     @pytest.mark.parametrize('cases', GenericCaseReader(ProjectPaths.ui_setup_case, process_ui_row).read())
-    def test_android_uu_case(self, cases):
+    def test_android_setup_case(self, cases):
         self.app.app_steps(cases)
 
     @pytest.mark.run(order=5)
     @pytest.mark.parametrize('cases', GenericCaseReader(ProjectPaths.ui_converter_case, process_ui_row).read())
-    def test_android_uu_case(self, cases):
+    def test_android_converter_case(self, cases):
         self.app.app_steps(cases)
