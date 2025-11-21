e89c1dcd8187b702a4caff4bdb82dcd0b8c2f6e4
Gui <9041355+ivogui@user.noreply.gitee.com>
2025-11-21 12:32:36 +0800

    1.配置修改

--- src/runners/__init__.py
+++ src/runners/__init__.py
@@ -3,4 +3,4 @@ from src.utils.platform_utils import clear_log_files, clear_directory
 
 clear_log_files(ProjectPaths.LOG_DIR)
 # clear_directory(ProjectPaths.CACHE_FILE)
-# clear_directory(ProjectPaths.REPORT_DIR)
+clear_directory(ProjectPaths.REPORT_DIR)
